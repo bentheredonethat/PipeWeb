0cc71f1f2d11dec574cbb69f4604b10563f4e895
Ben Levinsky <ben@levinsky.org>
2015-11-05 15:41:18 -0800

    now goes without dependencies

--- pipeweb.js
+++ pipeweb.js
@@ -28,57 +28,48 @@ var Instruction = function(instruction, registers, stage, num){
 
 
 function myCreateFunction() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(-1);
+	$(document).ready(function () {
+    table = document.getElementById("myTable");
+    row = table.insertRow(-1);
     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
-var cell5 = row.insertCell(4);
+var cell0 = row.insertCell(0);
+var cell1 = row.insertCell(1);
+var cell2 = row.insertCell(2);
+var cell3 = row.insertCell(3);
+var cell4 = row.insertCell(4);
+var cell5 = row.insertCell(5);
 
 var input = document.getElementById("myText").value;
 
-var cycleCounter = 1;
+cycleCounter  +=1;
 var inputArray = input.split(" ");
 var Instr1 = new Instruction(inputArray[0],inputArray.shift(), "pre", cycleCounter);
 
 // collection of stages
-var stages= {
-	"IF" : Instr1,
-	"ID": null,
-	"EX": null,
-	"MEM": null,
-	"WB": null	
-};
+var newStage =stages;
+
+// move stages
+newStage["WB"] = stages["MEM"];
+newStage["MEM"] = stages["EX"];
+newStage["EX"] = stages["ID"];
+newStage["ID"] = stages["IF"];
+newStage["IF"] = Instr1;
 
 // var dependencies
 var dependencies = null;
 
 // start cycle
-var pipe = new Pipeline(stages, dependencies,cycleCounter);
-
-var KeepGoing = 0;
-$.each(stages, function(index, value){
-	if (typeof value != null){
-		alert(value);
-		KeepGoing = 1;
-	}
-});
+var pipe = new Pipeline(newStage, dependencies,cycleCounter);
 
 // Add some text to the new cells:
+cell0.innerHTML = cycleCounter;
 cell1.innerHTML =  null != pipe.IF ? pipe.IF.operation:"";
 cell2.innerHTML =  null != pipe.ID ? pipe.ID.operation:"";
 cell3.innerHTML =  null != pipe.EX ? pipe.EX.operation:"";
 cell4.innerHTML =  null != pipe.MEM? pipe.MEM.operation:"";
 cell5.innerHTML =  null != pipe.WB ? pipe.WB.operation:"";
 
-// move each stage
-stages["WB"] = stages["MEM"];
-stages["MEM"] = stages["EX"];
-stages["EX"] = stages["ID"];
-stages["ID"] = stages["IF"];
-stages["ID"] = null;
+});
 
 
 }
