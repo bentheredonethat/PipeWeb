ddeec40123e4119a0c08fe9a8aade25a80b56a3d
Ben Levinsky <ben@levinsky.org>
2015-11-05 09:00:05 -0800

    and button for text to hooked up to

--- pipeweb.js
+++ pipeweb.js
@@ -1,19 +1,21 @@
-// process data here
-function processData() {
-   var jsonData = '[{"rank":"9","content":"Alon","UID":"5"},{"rank":"6","content":"Tala","UID":"6"}]';
+function myCreateFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(-1);
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
+var cell5 = row.insertCell(4);
 
-$.ajax({
-    url: '/echo/json/',
-    type: 'POST',
-    data: {
-        json: jsonData
-    },
-    success: function (response) {
-        var trHTML = '';
-        $.each(response, function (i, item) {
-            trHTML += '<tr><td>' + item.rank + '</td><td>' + item.content + '</td><td>' + item.UID + '</td></tr>';
-        });
-        $('#records_table').append(trHTML);
-    }
-});
-};
+// Add some text to the new cells:
+cell1.innerHTML = "NEW CELL1";
+cell2.innerHTML = "NEW CELL2";
+cell3.innerHTML = "NEW CELL3";
+cell4.innerHTML = "NEW CELL4";
+cell5.innerHTML = "NEW CELL5";
+}
+
+function doFunction(){
+    alert('clicked');
+}
